740877408908257c1c81995ba219af88606a8e98
LeCloutPanda <53411604+LeCloutPanda@users.noreply.github.com>
2023-09-27 21:29:15 +1000

    v1.0.1-a
    Untested

--- Patch.cs
+++ Patch.cs
@@ -13,8 +13,13 @@ namespace Resolutions
         public override string Version => "1.0.0";
 
         public static ModConfiguration config;
-        [AutoRegisterConfigKey] private static ModConfigurationKey<int2> RESOLUTION = new ModConfigurationKey<int2>("Finger Photo Resolution", "", () => new int2(1920, 1080));
-        [AutoRegisterConfigKey] private static ModConfigurationKey<int2> TiMER_RESOLUTION = new ModConfigurationKey<int2>("Finger Photo Timer Resolution", "", () => new int2(2560, 1440));
+        [AutoRegisterConfigKey] private static ModConfigurationKey<int2> RESOLUTION = new ModConfigurationKey<int2>("Photo Resolution", "", () => new int2(1920, 1080));
+        [AutoRegisterConfigKey] private static ModConfigurationKey<int2> TiMER_RESOLUTION = new ModConfigurationKey<int2>("Timer Photo Resolution", "", () => new int2(2560, 1440));
+        [AutoRegisterConfigKey] private static ModConfigurationKey<float> MIN_FOV = new ModConfigurationKey<float>("Min Fov", "", () => 20f);
+        [AutoRegisterConfigKey] private static ModConfigurationKey<float> MAX_FOV = new ModConfigurationKey<float>("Max Fov", "", () => 90f);
+        [AutoRegisterConfigKey] private static ModConfigurationKey<bool> DEBUG_GESTURE = new ModConfigurationKey<bool>("Enable Debug Gesture", "", () => false);
+        [AutoRegisterConfigKey] private static ModConfigurationKey<float> MIN_DISTANCE = new ModConfigurationKey<float>("Min Distance", "", () => 0.1f);
+        [AutoRegisterConfigKey] private static ModConfigurationKey<float> MAX_DISTANCE = new ModConfigurationKey<float>("Max Distance", "", () => 0.5f);
 
         public override void OnEngineInit()
         {
@@ -33,6 +38,11 @@ namespace Resolutions
                 if (__instance.Slot.ActiveUserRoot.ActiveUser != __instance.LocalUser) return;
                 __instance.NormalResolution.Value = config.GetValue(RESOLUTION);
                 __instance.TimerResolution.Value = config.GetValue(TiMER_RESOLUTION);
+                __instance.MinFOV.Value = config.GetValue(MIN_FOV);
+                __instance.MaxFOV.Value = config.GetValue(MAX_FOV);
+                __instance.DebugGesture.Value = config.GetValue(DEBUG_GESTURE);
+                __instance.MinDistance.Value = config.GetValue(MIN_DISTANCE);
+                __instance.MaxDistance.Value = config.GetValue(MAX_DISTANCE);
 
             }
         }
